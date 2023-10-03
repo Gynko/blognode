ecfefd28af8aeac963e42f48198a1663ebff5f64
Yoann Godiet <yoann.3d@gmail.com>
2023-10-03 08:50:45 +0200

    login page frontend

--- client/src/pages/login/login.page.jsx
+++ client/src/pages/login/login.page.jsx
@@ -1,10 +1,28 @@
 import Header from "../../components/header/header.component";
+import "./login.styles.css";
 
 export default function LoginPage() {
   return (
     <div>
       <Header />
-      <h1>Login Page</h1>
+      <section className="section-container">
+        <h1 className="login-title">Administrator Login</h1>
+        <form className="form">
+          <div className="input-container">
+            <label className="login-label" htmlFor="username">
+              Username
+            </label>
+            <input className="login-input" type="text" placeholder="Username" />
+          </div>
+          <div className="input-container">
+            <label className="login-label" htmlFor="password">
+              Password
+            </label>
+            <input className="login-input" type="text" placeholder="Password" />
+          </div>
+          <input className="login-submit" type="submit" />
+        </form>
+      </section>
     </div>
   );
 }
